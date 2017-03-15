357352d75b17458de06f17333ea4e1c99ee4ef3d
Daniel Segovia <daniel@dabici.mx>
2017-03-14 21:05:52 -0600

    fiddling around with chartjs, can display data.. next is to extract from from

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,19 +4,35 @@ var stocks = ['YHOO']
 module.exports = function(app){
 
 	app.get('/', function(req, res){
+		var today = new Date()
+		var year = today.getFullYear().toString()
+		var yearAgo = year - 1
+		var month = today.getMonth().toString()
+		var day = today.getDate().toString()
+		if(month.length < 2){
+			month = '0' + month
+		}
+		if(day.length < 2){
+			day = '0' + day
+		}
+		var todayComplete = year + '-' + month + '-' + day
+		var yearAgoComplete = yearAgo + '-' + month + '-' + day
+
 		yahooFinance.historical({
-			symbol: 'AAPL',
-			from: '2012-12-27',
-			to: '2012-12-31',
+			symbol: 'GOOGL',
+			from: yearAgoComplete,
+			to: todayComplete,
 			period: 'd'
 		}, function(err, quotes){
+			if(err){
+				console.error(err)
+			}
 			var stockPrice = []
 			var stockDate = []
 			for(i = 0; i < quotes.length; i++){
 				stockPrice.push(quotes[i].close)
 				stockDate.push(quotes[i].date)
 			}
-			console.log(stockPrice)
 			res.locals.stockPrice = stockPrice
 			res.locals.stockDate = stockDate
 			res.render('index')
--- public/js/chart.js
+++ public/js/chart.js
@@ -1,9 +1,28 @@
 var ctx = document.getElementById('myChart')
 
+stockPrice = stockPrice.split(',').map(Number)
+stockDate = stockDate.split(',')
+
 console.log(stockPrice)
 console.log(stockDate)
 
 var myChart = new Chart(ctx, {
-	type: 'line', 
-	data: stockPrice
+  type: 'line',
+  data: {
+    labels: stockDate,
+    datasets: [{
+      label: 'YHOO',
+      data: stockPrice,
+      backgroundColor: "rgba(153,255,51,0.4)"
+    }]
+  },
+  options:
+  {
+  	scales:
+  	{
+  		xAxes: [{
+  			display: false
+  		}]
+  	}
+  }
 })
