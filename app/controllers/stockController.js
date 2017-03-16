83d13f3245bebe79a7c45760603defe33281524e
Daniel Segovia <daniel@dabici.mx>
2017-03-15 21:49:33 -0600

    adding stock is now working, need to delete and validate that stock 1. is not in list, 2. that it exists

--- app/controllers/stockController.js
+++ app/controllers/stockController.js
@@ -1,57 +1,58 @@
 var yahooFinance = require('yahoo-finance')
 var stocks = []
+var stockPrices = []
+var stockDate = []
 
 function StockHandler() {
 	
 	this.showStocks = function(req, res){
-		console.log(stocks)
-		if(stocks.length > 0){
-			var today = new Date()
-			var year = today.getFullYear().toString()
-			var yearAgo = year - 1
-			var month = today.getMonth().toString()
-			var day = today.getDate().toString()
-			if(month.length < 2){
-				month = '0' + month
-			}
-			if(day.length < 2){
-				day = '0' + day
-			}
-			var todayComplete = year + '-' + month + '-' + day
-			var yearAgoComplete = yearAgo + '-' + month + '-' + day
-
-			yahooFinance.historical({
-				symbol: stocks[0],
-				from: yearAgoComplete,
-				to: todayComplete,
-				period: 'd'
-			}, function(err, quotes){
-				if(err){
-					console.error(err)
-				}
-				if(quotes.length > 0){
-					var stockPrice = []
-					var stockDate = []
-					for(i = 0; i < quotes.length; i++){
-						stockPrice.push(quotes[i].close)
-						stockDate.push(quotes[i].date)
-					}
-					res.locals.stockPrice = stockPrice
-					res.locals.stockDate = stockDate
-					res.locals.stocks = stocks
-					res.render('index')
-
-				}
-			})
-			
-		}else{
-			res.render('index')
-		}
+		// console.log(stocks)
+		// console.log(stockPrices)
+		// console.log(stockDate)
+		res.locals.stocks = stocks
+		res.locals.stockPrices = stockPrices
+		res.locals.stockDate = stockDate
+		res.render('index')	
 	}
 
 	this.addStock = function(req, res){
-		stocks.push(req.body.stock.toUpperCase())
-		res.redirect('/')
+		var addedStock = req.body.stock
+		var today = new Date()
+		var year = today.getFullYear().toString()
+		var yearAgo = year - 1
+		var month = today.getMonth().toString()
+		var day = today.getDate().toString()
+		if(month.length < 2){
+			month = '0' + month
+		}
+		if(day.length < 2){
+			day = '0' + day
+		}
+		var todayComplete = year + '-' + month + '-' + day
+		var yearAgoComplete = yearAgo + '-' + month + '-' + day
+
+		yahooFinance.historical({
+			symbol: addedStock,
+			from: yearAgoComplete,
+			to: todayComplete,
+			period: 'd'
+		}, function(err, quotes){
+			if(err){
+				console.error(err)
+			}
+			if(quotes.length > 0){
+				var stockPrice = []
+				stockDate = []
+				for(i = 0; i < quotes.length; i++){
+					stockPrice.push(quotes[i].close)
+					stockDate.push(quotes[i].date)
+				}
+				stocks.push(addedStock)
+				stockPrices.push(stockPrice)
+				res.redirect('/')
+
+			}
+		})
 	}
 
 	this.deleteStock = function(req, res){
--- public/js/chart.js
+++ public/js/chart.js
@@ -1,22 +1,33 @@
 var ctx = document.getElementById('myChart')
 
-stockPrice = stockPrice.split(',').map(Number)
-stockDate = stockDate.split(',')
-stocks = stocks.split(',')
+// stockPrice = stockPrice.split(',').map(Number)
+// stockDate = stockDate.split(',')
+// stocks = stocks.split(',')
 
-console.log(stockPrice)
-console.log(stockDate)
+
+var stockPrices = stockPrices.split(',').map(Number)
+var stockDate = stockDate.split(',')
+var stocks = stocks.split(',')
+console.log(stockPrices)
+console.log(stockDate.length)
 console.log(stocks)
+
+var dataSets = []
+
+for(i = 0; i < stocks.length; i++){
+	var stockPriceSliced = stockPrices.slice(i*stockDate.length, (i+1)*stockDate.length)
+	dataSets.push({
+		label: stocks[i],
+		data: stockPriceSliced,
+	  backgroundColor: color[i]
+	})
+}
 
 var myChart = new Chart(ctx, {
   type: 'line',
   data: {
     labels: stockDate,
-    datasets: [{
-      label: stocks,
-      data: stockPrice,
-      backgroundColor: "rgba(153,255,51,0.4)"
-    }]
+    datasets: dataSets
   },
   options:
   {
