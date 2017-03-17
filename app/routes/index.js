9bc31d3f07992389a6215324ca12a94f26e586b6
Daniel Segovia <daniel@dabici.mx>
2017-03-16 23:01:10 -0600

    everything is now working with sockets, except the chart... need to refresh it

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,89 @@
-var StockController = require('../controllers/stockController.js')
-var stockController = new StockController()
 
+var yahooFinance = require('yahoo-finance')
+var stocks = []
+var stockPrices = []
+var stockDate = []
 
 
 module.exports = function(app, io){
 
-	app.get('/', stockController.showStocks)
+	app.get('/', function(req, res){
+		res.locals.stocks = stocks
+		res.locals.stockPrices = stockPrices
+		res.locals.stockDate = stockDate
+		res.render('index')	
+	})
 
-	app.post('/', stockController.addStock)
-
-	app.get('/delete/:id', stockController.deleteStock)
-
-	io.on('connect', function(socket){
+	io.on('connection', function(socket){
 		console.log('User connected.')
-		io.on('disconnect', function(){
+
+		socket.on('new stock', function(data){
+		var addedStock = data.toUpperCase()
+		if(stocks.indexOf(addedStock) < 0){
+			var today = new Date()
+			var year = today.getFullYear().toString()
+			var yearAgo = year - 1
+			var month = today.getMonth() + 1
+			month = month.toString()
+			var day = today.getDate().toString()
+			if(month.length < 2){
+				month = '0' + month
+			}
+			if(day.length < 2){
+				day = '0' + day
+			}
+			var todayComplete = year + '-' + month + '-' + day
+			var yearAgoComplete = yearAgo + '-' + month + '-' + day
+
+			yahooFinance.historical({
+				symbol: addedStock,
+				from: yearAgoComplete,
+				to: todayComplete,
+				period: 'd'
+			}, function(err, quotes){
+				if(err){
+					console.error(err)
+				}
+				if(quotes.length > 0){
+					var stockPrice = []
+					stockDate = []
+					for(i = 0; i < quotes.length; i++){
+						stockPrice.push(quotes[i].close)
+						stockDate.push(quotes[i].date)
+					}
+					stocks.push(addedStock)
+					stockPrices.push(stockPrice)
+					io.emit('stock batch', {
+						stocks: stocks,
+						stockPrices: stockPrices,
+						stockDate: stockDate
+					})
+				}else{
+					socket.emit('flash', [ 'warning' , 'Invalid Stock: ' + addedStock ])
+				}
+			})
+
+		}else{
+			socket.emit('flash', [ 'warning' , 'Can\'t Duplicate Stock' ])
+		}
+	})
+
+		socket.on('delete', function(deleteId){
+			var stockToDel = deleteId
+			var stockToDelIndex = stocks.indexOf(stockToDel)
+			stocks.splice(stockToDelIndex, 1)
+			stockPrices.splice(stockToDelIndex, 1)
+			if(stocks.length < 1){
+				stockDate = []
+			}
+			io.emit('delete', deleteId)
+			socket.emit('flash', [ 'danger' , 'Deleted Stock: ' + stockToDel ])
+		})
+
+
+
+
+		socket.on('disconnect', function(){
 			console.log('User disconnected.')
 		})
 	})
--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,75 @@
 $(document).ready(function(){
 
-	$('article').click(function (){
+	$('#stock_form').submit(function(e){
+		e.preventDefault();
+	})
+
+	$('#flash_message').on('click', 'article' , function (){
 	  $(this).remove()
 	})
 
-	
-	
 })
+
+
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+
+
+$(function() {
+	var socket = io()
+
+	$('form').submit(function(){
+		var stockBox = $('#stock_box').val().toUpperCase()
+		$('#stock_box').val('')
+		socket.emit('new stock', stockBox)
+	})
+
+	$('#stock_list').on('click', '.delete_link', function(){
+		var stockIdToDel = $(this).parent().parent().attr('id')
+		socket.emit('delete', stockIdToDel)
+	})
+
+
+
+
+
+
+	socket.on('stock batch', function(data){
+		//rerender the chart with all the stocks
+
+		//add all stocks to the list
+		console.log(data.stocks)
+		$('#stock_list').append(
+			`<div id='${data.stocks[data.stocks.length-1]}' class='card'>
+				<div class='card-header'>
+					<p class='card-header-title'>${data.stocks[data.stocks.length-1]}</p>
+					<a class='card-header-icon delete_link'>
+						<span class='icon'>
+							<i class='fa fa-times'></i>
+						</span>
+					</a>
+				</div>
+			</div>`
+			)
+	})
+
+	socket.on('delete', function(deleteId){
+		$(`#${deleteId}`).remove()
+	})
+
+
+	socket.on('flash', function(message){
+		$('#flash_message').append(
+			`<article class='message is-${message[0]}'>
+				<div class='message-header'>
+					<p>${message[1]}</p>
+					<button class='delete'></button>
+				</div>
+			</article>`
+			)
+	})
+
+
+})
+
+
